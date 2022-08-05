065efd029f0bd57a55cdc43008fdb3c2ff47c1cf
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-08-05 10:27:24 -0400

    feat: structure of the project

---
+++ .exlintrc.js
@@ -0,0 +1,11 @@
+module.export = {
+  ....
+  overrides:[
+    {
+      files: ['src/components/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0
+      }
+    }
+  ]
+}
---
+++ src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>asassdasdas</div>
+</template>
