fb041e17658e8e928a83cd380782d262d873def1
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-08-21 11:30:08 -0400

    feat: header page

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header-page">
+    <h1 class="title-page">DrawFlow - Learn to programming</h1>
+  </header>
+</template>
+
+<style scoped>
+.header-page {
+  width: 100%;
+  height: 80px;
+  border-bottom: 1px solid gray;
+  box-shadow: 0 1px 5px gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.title-page {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+</style>
